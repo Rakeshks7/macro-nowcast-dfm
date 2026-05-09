febf844d3ce9cdd4d82d45cdab382cb90dddc6ca
Rakesh K <rakeshdraft4@gmail.com>
2026-05-09 20:08:26 +0530

    Add files via upload

---
+++ src/config.py
@@ -0,0 +1,10 @@
+INDICATORS = {
+    'GDPC1': {'name': 'Real GDP', 'freq': 'Q', 'transform': 5},
+    'PAYEMS': {'name': 'Nonfarm Payrolls', 'freq': 'M', 'transform': 5},
+    'INDPRO': {'name': 'Industrial Production', 'freq': 'M', 'transform': 5},
+    'CPIAUCSL': {'name': 'CPI', 'freq': 'M', 'transform': 5},
+    'RETAILx': {'name': 'Retail Sales', 'freq': 'M', 'transform': 5},
+}
+
+START_DATE = '2005-01-01'
+BASE_FREQ = 'M'
